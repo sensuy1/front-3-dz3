4d3ed2734ef4e27cb4c67e5e0b2519cdb2294c91
sensuy1 <150375998+sensuy1@users.noreply.github.com>
2023-12-16 13:40:33 +0600

    Add files via upload

---
+++ project_program/project_program/js/lesson.js
@@ -0,0 +1,66 @@
+// PHONE CHECKER
+
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
+
+const regExp = /^\+996 [25793]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.onclick = () => {
+    if(regExp.test(phoneInput.value)){
+        phoneResult.innerHTML = "OK"
+        phoneResult.style.color = "green"
+    }else{
+        phoneResult.innerHTML = "NOT OK"
+        phoneResult.style.color = "red"
+    }
+}
+
+
+
+// TAB SLIDER
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach(tabCard => {
+        tabCard.style.display = 'none'
+    })
+    tabs.forEach(tab => {
+        tab.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (tabIndex = 0) => {
+    tabContentBlocks[tabIndex].style.display = 'block'
+    tabs[tabIndex].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabs.forEach((tab,tabIndex) => {
+            if (event.target === tab) {
+                hideTabContent()
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+
+// HOMEWORK 3 PART 1
+const autoSlider = (index = 0) => {
+    setInterval(() => {
+        index++
+        if (index > tabs.length - 1) {
+            index = 0
+        }
+        hideTabContent()
+        showTabContent(index)
+    }, 3500)
+}
+autoSlider()
